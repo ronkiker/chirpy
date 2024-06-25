75dfefaa8382c30c5ea717a35c799061953c61cd
ronkiker <ron.s.kiker@gmail.com>
2024-06-25 17:07:46 -0600

    add authenticate

---
+++ authenticate/authenticate.go
@@ -0,0 +1,76 @@
+package authenticate
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var ErrNoAuthHeaderIncluded = errors.New("not auth header included in request")
+
+func HashPassword(password string) (string, error) {
+	data, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
+}
+
+func CheckPassword(password, hash string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+}
+
+func CreateJWT(userId int, secret string, expires time.Duration) (string, error) {
+	key := []byte(secret)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    "chirpy",
+		IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(expires)),
+		Subject:   fmt.Sprintf("%d", userId),
+	})
+	return token.SignedString(key)
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (string, error) {
+	claimsStruct := jwt.RegisteredClaims{}
+	token, err := jwt.ParseWithClaims(
+		tokenString,
+		&claimsStruct,
+		func(token *jwt.Token) (interface{}, error) { return []byte(tokenSecret), nil },
+	)
+	if err != nil {
+		return "", err
+	}
+
+	userIdString, err := token.Claims.GetSubject()
+	if err != nil {
+		return "", err
+	}
+
+	issuer, err := token.Claims.GetIssuer()
+	if err != nil {
+		return "", err
+	}
+
+	if issuer != string("chirpy") {
+		return "", errors.New("invalid issuer")
+	}
+	return userIdString, nil
+}
+
+func GetBearer(headers http.Header) (string, error) {
+	auth := headers.Get("Authorization")
+	if auth == "" {
+		return "", errors.New("no authorization header")
+	}
+	parseAuth := strings.Split(auth, " ")
+	if len(parseAuth) < 2 || parseAuth[0] != "Bearer" {
+		return "", errors.New("malformed authorization header")
+	}
+	return parseAuth[1], nil
+}
